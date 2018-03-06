f90d3da0b911cbc90d0e8ca0c02f4b71b9f9a4b2
Vlasko <luka.j.v@gmail.com>
2018-03-06 16:52:21 +0000

    Merge branch 'master' of https://github.com/Vlasko/MPhys
    Co-Authored-By: charlottecooke <charlottecooke@users.noreply.github.com>

--- Matrix_Model.py
+++ Matrix_Model.py
@@ -1,23 +1,23 @@
-from numpy import cos, inf, zeros, array, exp, conj, nan, isnan, pi, sin
-
 import numpy as np
-import time
+from numpy import cos, sin, exp
+
+z = 0 + 1j
+# define the imagineary number
+
+# CONSTANTS
+e = 1.602 * 10 ** -19
+# fundamental charge in C
+eps_0 = 8.85 * 10 ** -12
+# permittivity of the vacuum in C^2 N^-1 m^-2
+m = 9.11 * 10 ** -31
+# mass of electron in kg
+h_bar = 6.58 * 10 ** -16
+# reduced plank constant in eVs
+c = 3*10**8
 
 # for sodium
 n = 2.54 * 10 ** 28
 # number density of electrons in m^-3
-
-e = 1.602 * 10 ** -19
-# fundamental charge in C
-
-eps_0 = 8.85 * 10 ** -12
-# permittivity of the vacuum in C^2 N^-1 m^-2
-
-m = 9.11 * 10 ** -31
-# mass of electron in kg
-
-h_bar = 6.58 * 10 ** -16
-# reduced plank constant in eVs
 
 steps = int(10/0.1) + 1
 w = np.empty((steps))
@@ -34,21 +34,17 @@ plas_w = np.sqrt((n*e**2)/(eps_0*m))
 plas_w_eV = plas_w * h_bar
 
 N = np.sqrt( 1 - (plas_w_eV**2/w**2))
+# complex refractive index
 
-# for w in range(len(w)):
-#     if w > plas_w_eV:
-#         N = np.sqrt( 1 - (plas_w_eV**2/w**2))
-#     else:
-#         N= 0
-# complex refractive index (from Textbook)
+theta = 0
+cos_theta = np.cos(theta)
+# at the end we should integrate through all angles, for the moment focus on incident.
 
-R = np.absolute((1-N)/(1+N)) **2
-# relfection coefficient
+p = N*cos_theta
 
-import matplotlib.pyplot as plt
+k_0 = w/c
 
-# plot a graph of time against current
-plt.plot(w,R)
-plt.xlabel('Photon Energy (eV)')
-plt.ylabel('Reflection')
-plt.grid(True)
+d = 200
+# depth of layer in nm
+
+M = np.matrix( [cos(k_0 * N * d * cos_theta)   (-z/p)*sin(k_0 * N * d * cos_theta) ], [-zp*sin(k_0 * N * d * cos_theta) , cos(k_0 * N * z * cos_theta)] )
